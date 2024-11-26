70aac18ce948d6a23acca1486e694d3d566e6b5a
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-26 15:22:38 -0500

    Add library file path documentation
    Included library file relative path as a comment for context. This aids in understanding the linkage details and ensures the `link` macro usage is clear.

--- src-rust/src/cpplink.rs
+++ src-rust/src/cpplink.rs
@@ -1,4 +1,9 @@
+//! Library File relative path:
+//! ../src-cpp/bin/obj/release/libsrc_cpp-Windows-AMD64.lib
+//! The `link` macro will automatically add the .lib to the end of the name
+
+
 #[link(name = "libsrc_cpp-Windows-AMD64", kind = "static")]
 extern "C" {
-	pub fn hello();
+    pub fn hello();
 }
