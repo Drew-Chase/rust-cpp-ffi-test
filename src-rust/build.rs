78bb0c1b42eef2226c89abec1d8ccf7aae606208
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-26 15:22:30 -0500

    Add build script for compiling C++ library with CMake
    This commit introduces a new `build.rs` script in the Rust project. The script uses CMake to compile a C++ library and specifies the path and static linking details for integration with Rust. The code also contains commented-out sections for linking the standard C++ library depending on the target platform.

---
+++ src-rust/build.rs
@@ -0,0 +1,35 @@
+use cmake::Config;
+use std::env;
+
+fn main() {
+    // Use CMake to compile the C++ library
+    let dst = Config::new("../src-cpp")
+        .no_build_target(true)
+        .configure_arg("/NODEFAULTLIB:library")
+        .build();
+
+    // The path to the actual directory containing the compiled library
+    let out_path = dst.join("build/Debug");
+
+    println!("Path to the compiled library: {}", out_path.display());
+
+    // Output directory containing the compiled library
+    println!("cargo:rustc-link-search=native={}", out_path.display());
+
+    // Link the static library with the exact name
+    println!("cargo:rustc-link-lib=static=libsrc_cpp-Windows-AMD64");
+
+    // Link the C++ standard library
+//    let target = env::var("TARGET").unwrap();
+//    if target.contains("msvc") {
+//        // Windows Microsoft Visual C/C++ Compiler (MSVC)
+//        println!("cargo:rustc-link-lib=dylib=msvcrt");
+//    } else if target.contains("gnu") {
+//        // Unix/Linux GNU C/C++ Compiler (GCC)
+//        println!("cargo:rustc-link-lib=dylib=stdc++");
+//    } else if target.contains("apple") {
+//        // Darwin/MacOS C/C++ Compiler
+//        // This uses XCode's CLANG Compiler
+//        println!("cargo:rustc-link-lib=dylib=c++");
+//    }
+}
